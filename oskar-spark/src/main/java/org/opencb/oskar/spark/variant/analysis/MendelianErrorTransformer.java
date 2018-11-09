eaf1e0fb143e4043bf35c5c4ba4e521c3b36c28b
Jacobo Coll Moragón <jacobo167@gmail.com>
2018-11-09 11:10:26 +0000

    spark: Improve MendelianErrorTransformer. Add dummy test.

--- oskar-spark/src/main/java/org/opencb/oskar/spark/variant/analysis/MendelianErrorTransformer.java
+++ oskar-spark/src/main/java/org/opencb/oskar/spark/variant/analysis/MendelianErrorTransformer.java
@@ -1,134 +1,156 @@
 package org.opencb.oskar.spark.variant.analysis;
 
-import org.apache.spark.ml.Transformer;
+import org.apache.commons.lang.StringUtils;
 import org.apache.spark.ml.param.Param;
-import org.apache.spark.ml.param.ParamMap;
-import org.apache.spark.ml.util.Identifiable$;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.expressions.UserDefinedFunction;
-import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.opencb.biodata.models.feature.AllelesCode;
 import org.opencb.biodata.models.feature.Genotype;
+import org.opencb.oskar.spark.variant.Oskar;
 import scala.collection.mutable.ListBuffer;
 import scala.runtime.AbstractFunction4;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.udf;
+import static org.apache.spark.sql.types.DataTypes.*;
 
 /**
  * Created on 12/06/18.
  *
+ * Using Plink Mendel error codes
+ * https://www.cog-genomics.org/plink2/basic_stats#mendel
+ *
  * @author Jacobo Coll &lt;jacobo167@gmail.com&gt;
  */
-public class MendelianErrorTransformer extends Transformer {
+public class MendelianErrorTransformer extends AbstractTransformer {
 
-
-    private String uid;
-//    private Param<String> cohortParam;
     private Param<String> studyIdParam;
+    private Param<String> fatherParam;
+    private Param<String> motherParam;
+    private Param<String> childParam;
 
     public MendelianErrorTransformer() {
         this(null);
     }
 
     public MendelianErrorTransformer(String uid) {
-        this.uid = uid;
-//        setDefault(cohortParam(), "ALL");
+        super(uid);
+        fatherParam = new Param<>(this, "father", "");
+        motherParam = new Param<>(this, "mother", "");
+        childParam = new Param<>(this, "child", "");
+        studyIdParam = new Param<>(this, "studyId", "");
         setDefault(studyIdParam(), "");
     }
-//
-//    public Param<String> cohortParam() {
-//        return cohortParam = cohortParam == null ? new Param<>(this, "cohort", "") : cohortParam;
-//    }
-//
-//    public MendelianErrorTransformer setCohort(String cohort) {
-//        set(cohortParam, cohort);
-//        return this;
-//    }
-//
-//    public String getCohort() {
-//        return getOrDefault(cohortParam);
-//    }
 
     public Param<String> studyIdParam() {
-        return studyIdParam = studyIdParam == null ? new Param<>(this, "studyId", "") : studyIdParam;
+        return studyIdParam;
     }
 
     public MendelianErrorTransformer setStudyId(String studyId) {
-        set(studyIdParam, studyId);
+        set(studyIdParam(), studyId);
         return this;
     }
 
     public String getStudyId() {
-        return getOrDefault(studyIdParam);
+        return getOrDefault(studyIdParam());
     }
 
-    @Override
-    public String uid() {
-        return getUid();
+    public Param<String> fatherParam() {
+        return fatherParam;
     }
 
-    private String getUid() {
-        if (uid == null) {
-            uid = Identifiable$.MODULE$.randomUID("VariantStatsTransformer");
-        }
-        return uid;
+    public MendelianErrorTransformer setFather(String father) {
+        set(fatherParam, father);
+        return this;
     }
 
+    public String getFather() {
+        return getOrDefault(fatherParam);
+    }
+
+    public Param<String> motherParam() {
+        return motherParam;
+    }
+
+    public MendelianErrorTransformer setMother(String mother) {
+        set(motherParam, mother);
+        return this;
+    }
+
+    public String getMother() {
+        return getOrDefault(motherParam);
+    }
+
+    public Param<String> childParam() {
+        return childParam;
+    }
+
+    public MendelianErrorTransformer setChild(String child) {
+        set(childParam, child);
+        return this;
+    }
+
+    public String getChild() {
+        return getOrDefault(childParam);
+    }
     @Override
     public Dataset<Row> transform(Dataset<?> dataset) {
+        Dataset<Row> df = (Dataset<Row>) dataset;
+
+        UserDefinedFunction mendelianUdf = udf(new MendelianErrorFunction("X", "Y"), IntegerType);
+
+        List<String> samples;
+        Map<String, List<String>> samplesMap = new Oskar().samples(df);
+        // FIXME: Dataset can be multi-study
+        if (StringUtils.isEmpty(getStudyId())) {
+            samples = samplesMap.entrySet().iterator().next().getValue();
+        } else {
+            samples = samplesMap.get(getStudyId());
+        }
+        String father = getFather();
+        String mother = getMother();
+        String child = getChild();
 
 
-        Dataset<Row> datasetRow = (Dataset<Row>) dataset;
-//
-//        datasetRow.withColumn("studies[0].stats", new Column("studies[0].stats"));
-//
-//        UserDefinedFunction mode = udf((Function1<Row, String>) (Row ss) -> "", DataTypes.StringType);
-//
-//        datasetRow.select(mode.apply(col("vs"))).show();
+        int fatherIdx = samples.indexOf(father);
+        int motherIdx = samples.indexOf(mother);
+        int childIdx = samples.indexOf(child);
 
-//        datasetRow.map((MapFunction<Row, Row>) row -> {
-//
-//            Row study = (Row) row.getList(row.fieldIndex("studies")).get(0);
-//            List<List<String>> samplesData = RowToVariantConverter.getSamplesData(study);
-//
-////            samplesData.get()
-//
-//            return row;
-//        }, null);
-
-//
-//        StructType schema = datasetRow.schema();
-//        schema.schema.getFieldIndex("studies").
-
-//        UserDefinedFunction stats = udf(new VariantStatsFunction(), STATS_MAP_DATA_TYPE);
-        UserDefinedFunction statsFromStudy = udf(new MendelianErrorFunction(), DataTypes.IntegerType);
-
-        return datasetRow.withColumn("code", statsFromStudy.apply(new ListBuffer<Column>()
+        return df.withColumn("code", mendelianUdf.apply(new ListBuffer<Column>()
                 .$plus$eq(col("chromosome"))
-                .$plus$eq(col("studies").getItem(0).getField("samplesData").getItem(0).getItem(0))
-                .$plus$eq(col("studies").getItem(0).getField("samplesData").getItem(1).getItem(0))
-                .$plus$eq(col("studies").getItem(0).getField("samplesData").getItem(2).getItem(0))
+                .$plus$eq(col("studies").getItem(0).getField("samplesData").getItem(fatherIdx).getItem(0))
+                .$plus$eq(col("studies").getItem(0).getField("samplesData").getItem(motherIdx).getItem(0))
+                .$plus$eq(col("studies").getItem(0).getField("samplesData").getItem(childIdx).getItem(0))
         ));
     }
 
     @Override
     public StructType transformSchema(StructType schema) {
-        return schema;
-    }
-
-    @Override
-    public Transformer copy(ParamMap extra) {
-        return defaultCopy(extra);
+        List<StructField> fields = Arrays.stream(schema.fields()).collect(Collectors.toList());
+        fields.add(createStructField("code", IntegerType, false));
+        return createStructType(fields);
     }
 
     public static class MendelianErrorFunction
             extends AbstractFunction4<String, String, String, String, Integer> implements Serializable {
+
+        private String chrX;
+        private String chrY;
+
+        public MendelianErrorFunction(String chrX, String chrY) {
+            this.chrX = chrX;
+            this.chrY = chrY;
+        }
 
         private enum GenotypeCode {
             HOM_REF, HOM_VAR, HET
@@ -148,9 +170,11 @@ public class MendelianErrorTransformer extends Transformer {
                 GenotypeCode motherCode = getAlternateAlleleCount(motherGt);
                 GenotypeCode childCode = getAlternateAlleleCount(childGt);
 
-                if (chromosome.equals("X")) {
+                if (chromosome.equals(chrX)) {
+                    // TODO
                     code = 0;
-                } else if (chromosome.equals("Y")) {
+                } else if (chromosome.equals(chrY)) {
+                    // TODO
                     code = 0;
                 } else {
                     if (childCode == GenotypeCode.HET) {
@@ -205,7 +229,7 @@ public class MendelianErrorTransformer extends Transformer {
                 case 1:
                     return GenotypeCode.HET;
                 default:
-                    return GenotypeCode.HOM_REF;
+                    return GenotypeCode.HOM_VAR;
             }
         }
     }
---
+++ oskar-spark/src/test/java/org/opencb/oskar/spark/variant/analysis/MendelianErrorTransformerTest.java
@@ -0,0 +1,33 @@
+package org.opencb.oskar.spark.variant.analysis;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.opencb.oskar.spark.OskarSparkTestUtils;
+import org.opencb.oskar.spark.commons.OskarException;
+
+import java.io.IOException;
+
+import static org.apache.spark.sql.functions.col;
+
+/**
+ * Created on 09/11/18.
+ *
+ * @author Jacobo Coll &lt;jacobo167@gmail.com&gt;
+ */
+public class MendelianErrorTransformerTest {
+
+    @ClassRule
+    public static OskarSparkTestUtils sparkTest = new OskarSparkTestUtils();
+
+    @Test
+    public void testMendelianErrors() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        new MendelianErrorTransformer().setStudyId("hgvauser@platinum:illumina_platinum").setFather("NA12877").transform(df)
+//                .where("code != 0").show();
+                .where(col("code").notEqual(0))
+                .show();
+    }
+}
