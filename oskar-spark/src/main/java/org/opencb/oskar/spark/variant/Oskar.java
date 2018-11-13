8fc3615181b44cfe920de6c4462ed43abece2cde
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2018-11-13 10:23:41 +0000

    spark: implement pedigree support in Spark metadata

--- oskar-spark/src/main/java/org/opencb/oskar/spark/variant/Oskar.java
+++ oskar-spark/src/main/java/org/opencb/oskar/spark/variant/Oskar.java
@@ -1,12 +1,17 @@
 package org.opencb.oskar.spark.variant;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.*;
+import org.opencb.biodata.models.clinical.pedigree.Member;
+import org.opencb.biodata.models.clinical.pedigree.Pedigree;
+import org.opencb.biodata.models.commons.Phenotype;
 import org.opencb.biodata.models.metadata.Individual;
 import org.opencb.biodata.models.metadata.Sample;
+import org.opencb.biodata.models.pedigree.Multiples;
 import org.opencb.biodata.models.variant.metadata.VariantMetadata;
 import org.opencb.biodata.models.variant.metadata.VariantStudyMetadata;
 import org.opencb.commons.utils.FileUtils;
@@ -120,35 +125,63 @@ public class Oskar {
 
     private Metadata createDatasetMetadata(VariantMetadata variantMetadata) {
         Map<String, List<String>> samplesMap = new HashMap<>();
-        Map<String, List<String>> phenotypesMap = new HashMap<>();
+        Map<String, Map<String, Map<String, Metadata>>> pedigreeMap = new HashMap<>();
         for (VariantStudyMetadata study: variantMetadata.getStudies()) {
+            if (!pedigreeMap.containsKey(study.getId())) {
+                pedigreeMap.put(study.getId(), new HashMap<>());
+            }
             List<String> samples = new ArrayList<>();
-            List<String> phenotypes = new ArrayList<>();
+
             for (Individual individual : study.getIndividuals()) {
+                String family = individual.getFamily();
+                if (!pedigreeMap.get(study.getId()).containsKey(family)) {
+                    pedigreeMap.get(study.getId()).put(family, new HashMap<>());
+                }
+
                 String phenotype = individual.getPhenotype() == null ? "" : individual.getPhenotype();
+                String sex = individual.getSex() == null ? "" : individual.getSex();
+                String mother = individual.getMother() == null ? "" : individual.getMother();
+                String father = individual.getFather() == null ? "" : individual.getFather();
+
                 for (Sample sample : individual.getSamples()) {
                     samples.add(sample.getId());
-                    // Set phenotype to all samples of that individual
-                    phenotypes.add(phenotype);
+
+                    Metadata attrMetadata = new MetadataBuilder()
+                            .putString("phenotype", phenotype)
+                            .putString("sex", sex)
+                            .putString("mother", mother)
+                            .putString("father", father)
+                            .build();
+
+                    pedigreeMap.get(study.getId()).get(family).put(sample.getId(), attrMetadata);
                 }
             }
             samplesMap.put(study.getId(), samples);
-            phenotypesMap.put(study.getId(), phenotypes);
         }
 
+        // Sample management
         MetadataBuilder samplesMetadata = new MetadataBuilder();
         for (Map.Entry<String, List<String>> entry : samplesMap.entrySet()) {
             samplesMetadata.putStringArray(entry.getKey(), entry.getValue().toArray(new String[0]));
         }
 
-        MetadataBuilder phenotypesMetadata = new MetadataBuilder();
-        for (Map.Entry<String, List<String>> entry : phenotypesMap.entrySet()) {
-            phenotypesMetadata.putStringArray(entry.getKey(), entry.getValue().toArray(new String[0]));
+        // Pedigree management
+        MetadataBuilder pedigreeMetadata = new MetadataBuilder();
+        for (String studyId: pedigreeMap.keySet()) {
+            MetadataBuilder familyMetadata = new MetadataBuilder();
+            for (String familyId: pedigreeMap.get(studyId).keySet()) {
+                MetadataBuilder sampleIdMetadata = new MetadataBuilder();
+                for (String sampleId: pedigreeMap.get(studyId).get(familyId).keySet()) {
+                    sampleIdMetadata.putMetadata(sampleId, pedigreeMap.get(studyId).get(familyId).get(sampleId));
+                }
+                familyMetadata.putMetadata(familyId, sampleIdMetadata.build());
+            }
+            pedigreeMetadata.putMetadata(studyId, familyMetadata.build());
         }
 
         return new MetadataBuilder()
                 .putMetadata("samples", samplesMetadata.build())
-                .putMetadata("phenotypes", phenotypesMetadata.build())
+                .putMetadata("pedigree", pedigreeMetadata.build())
                 .build();
     }
 
@@ -182,34 +215,109 @@ public class Oskar {
                 .metadata().getMetadata("samples");
     }
 
-    // Phenotypes
-    public Map<String, List<String>> phenotypes(Dataset<Row> df) {
-        Metadata phenotypesMetadata = getPhenotypesMetadata(df);
+    // Pedigree management
+    public Map<String, List<Pedigree>> pedigree(Dataset<Row> df) {
+        // Pedigree metadata = Map<study ID, family metadata>
+        Metadata pedigreeMetadata = getPedigreeMetadata(df);
 
-        Map<String, List<String>> map = new HashMap<>();
-        Iterator<String> it = phenotypesMetadata.map().keysIterator();
-        while (it.hasNext()) {
-            String studyId = it.next();
-            String[] phenotypeNames = phenotypesMetadata.getStringArray(studyId);
-            map.put(studyId, Arrays.asList(phenotypeNames));
+        Map<String, List<Pedigree>> pedigreeMap = new HashMap<>();
+        Iterator<String> studyIt = pedigreeMetadata.map().keysIterator();
+        while (studyIt.hasNext()) {
+            String studyId = studyIt.next();
+            pedigreeMap.put(studyId, new ArrayList<>());
+
+            // Family metadata = Map<family name, sample metadata>
+            Metadata familyMetadata = pedigreeMetadata.getMetadata(studyId);
+            Iterator<String> familyIt = familyMetadata.map().keysIterator();
+            while (familyIt.hasNext()) {
+                String familyName = familyIt.next();
+                Pedigree pedigree = new Pedigree(familyName, new ArrayList<>(), new HashMap<>());
+
+                // Map to contain all member of a pedigree/family, and siblings
+                Map<String, Member> membersMap = new HashMap<>();
+                Map<String, List<String>> siblingsMap = new HashMap<>();
+
+                // sample metadata = Map<sample ID, attribute metadata>
+                Metadata sampleMetadata = familyMetadata.getMetadata(familyName);
+                Iterator<String> sampleIt = sampleMetadata.map().keysIterator();
+                while (sampleIt.hasNext()) {
+                    String sampleId = sampleIt.next();
+                    if (!membersMap.containsKey(sampleId)) {
+                        membersMap.put(sampleId, new Member().setId(sampleId));
+                    }
+                    Member member = membersMap.get(sampleId);
+
+                    Metadata attrMetadata = sampleMetadata.getMetadata(sampleId);
+                    String fatherId = null;
+                    String motherId = null;
+                    // Father
+                    if (attrMetadata.contains("father")) {
+                        fatherId = attrMetadata.getString("father");
+                        if (!membersMap.containsKey(fatherId)) {
+                            Member father = new Member().setId(fatherId);
+                            membersMap.put(fatherId, father);
+                        }
+                        member.setFather(membersMap.get(fatherId));
+                    }
+
+                    // Mother
+                    if (attrMetadata.contains("mother")) {
+                        motherId = attrMetadata.getString("mother");
+                        if (!membersMap.containsKey(motherId)) {
+                            Member mother = new Member().setId(motherId);
+                            membersMap.put(motherId, mother);
+                        }
+                        member.setMother(membersMap.get(motherId));
+                    }
+
+                    if (StringUtils.isNotEmpty(fatherId) && StringUtils.isNotEmpty(motherId)) {
+                        String id = fatherId + "_" + motherId;
+                        if (!siblingsMap.containsKey(id)) {
+                            siblingsMap.put(id, new ArrayList<>());
+                        }
+                        siblingsMap.get(id).add(sampleId);
+                    }
+
+                    // Other attributes
+                    member.setSex(Member.Sex.getEnum(attrMetadata.getString("sex")));
+                    member.setPhenotypes(Collections.singletonList(new Phenotype(attrMetadata.getString("phenotype"),
+                            attrMetadata.getString("phenotype"), null)));
+
+                    // Finally, add member to the pedigreee/family
+                    pedigree.getMembers().add(member);
+                }
+
+                // Update siblings
+                for (Map.Entry<String, Member> entry: membersMap.entrySet()) {
+                    if (entry.getValue().getFather() != null & entry.getValue().getMother() != null
+                            && entry.getValue().getFather().getId() != null & entry.getValue().getMother().getId() != null) {
+                        String id = entry.getValue().getFather().getId() + "_" + entry.getValue().getMother().getId();
+                        if (siblingsMap.containsKey(id)) {
+                            Multiples multiples = new Multiples(null, new ArrayList<>());
+                            for (String siblingId: siblingsMap.get(id)) {
+                                if (!siblingId.equals(entry.getKey())) {
+                                    multiples.getSiblings().add(siblingId);
+                                }
+                            }
+                            membersMap.get(entry.getKey()).setMultiples(multiples);
+                        }
+                    }
+                }
+
+                // And, add pedigree to study
+                pedigreeMap.get(studyId).add(pedigree);
+            }
         }
-
-        return map;
+        return pedigreeMap;
     }
 
-    public List<String> phenotypes(Dataset<Row> df, String studyId) throws OskarException {
-        Metadata phenotypesMetadata = getPhenotypesMetadata(df);
-
-        String[] phenotypeNames = phenotypesMetadata.getStringArray(studyId);
-        if (phenotypeNames == null) {
-            throw OskarException.unknownStudy(studyId, scala.collection.JavaConversions.mapAsJavaMap(phenotypesMetadata.map()).keySet());
-        }
-        return Arrays.asList(phenotypeNames);
+    public List<Pedigree> pedigree(Dataset<Row> df, String studyId) throws OskarException {
+        return pedigree(df).get(studyId);
     }
 
-    private Metadata getPhenotypesMetadata(Dataset<Row> df) {
+    private Metadata getPedigreeMetadata(Dataset<Row> df) {
         return ((StructType) ((ArrayType) df.schema().apply("studies").dataType()).elementType()).apply("samplesData")
-                .metadata().getMetadata("phenotypes");
+                .metadata().getMetadata("pedigree");
     }
 
     public Dataset<Row> stats(Dataset<Row> df, String studyId, String cohort, List<String> samples) {
