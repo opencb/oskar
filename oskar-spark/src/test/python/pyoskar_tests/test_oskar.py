2dfbf04dae6192e13d38c5559df1c07168da8e6e
Sergio Roldán <sergioroldan96@gmail.com>
2018-11-22 17:17:25 +0000

    spark: set snake case on UDF definition and test

--- oskar-spark/src/test/python/pyoskar_tests/test_oskar.py
+++ oskar-spark/src/test/python/pyoskar_tests/test_oskar.py
@@ -40,13 +40,13 @@ class TestOskar(TestCase):
     def test_filter_study(self):
         self.df.selectExpr("studies[0].files.fileId").show(10, False)
 
-        self.df.select(fileQual("studies", "platinum-genomes-vcf-NA12877_S1.genome.vcf.gz").alias("qual")).show(10)
+        self.df.select(file_qual("studies", "platinum-genomes-vcf-NA12877_S1.genome.vcf.gz").alias("qual")).show(10)
         self.df.selectExpr("fileQual(studies, 'platinum-genomes-vcf-NA12877_S1.genome.vcf.gz')").show(10)
 
     def test_filter_annot(self):
-        self.df.withColumn("freqs", populationFrequencyAsMap("annotation")).where("freqs['1kG_phase3:ALL'] > 0").show(
+        self.df.withColumn("freqs", population_frequency_as_map("annotation")).where("freqs['1kG_phase3:ALL'] > 0").show(
             10)
         self.df.select(genes("annotation").alias("genes")).where("genes[0] is not null").show(10)
-        self.df.withColumn("freqs", populationFrequencyAsMap("annotation")).show(100)
-        self.df.withColumn("freqs", populationFrequencyAsMap("annotation")).where("freqs['1kG_phase3:ALL'] > 0").show(
+        self.df.withColumn("freqs", population_frequency_as_map("annotation")).show(100)
+        self.df.withColumn("freqs", population_frequency_as_map("annotation")).where("freqs['1kG_phase3:ALL'] > 0").show(
             100)
