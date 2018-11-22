9bdbb6e004b8d47df948e18cd18c4ba4410e99d6
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2018-11-22 13:34:51 +0000

    spark: implement facet transformer

---
+++ oskar-spark/src/main/java/org/opencb/oskar/spark/variant/analysis/FacetTransformer.java
@@ -0,0 +1,362 @@
+package org.opencb.oskar.spark.variant.analysis;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.ml.param.Param;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
+import org.apache.spark.sql.expressions.UserDefinedFunction;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.MetadataBuilder;
+import org.apache.spark.sql.types.StructType;
+import org.opencb.commons.utils.ListUtils;
+import scala.collection.mutable.ListBuffer;
+import scala.collection.mutable.WrappedArray;
+import scala.runtime.AbstractFunction1;
+
+import java.io.Serializable;
+import java.security.InvalidParameterException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.apache.spark.sql.functions.*;
+import static org.opencb.oskar.spark.variant.udf.VariantUdfManager.biotypes;
+import static org.opencb.oskar.spark.variant.udf.VariantUdfManager.genes;
+
+public class FacetTransformer extends AbstractTransformer {
+
+
+    public enum FacetType {
+        CATEGORICAL,
+        RANGE,
+        AGGREGATION
+    }
+
+    public static final String FACET_SEPARATOR = ";";
+    public static final String LABEL_SEPARATOR = "___";
+    private static final String NESTED_FACET_SEPARATOR = ">>";
+    private static final String NESTED_SUBFACET_SEPARATOR = ",";
+    public static final String INCLUDE_SEPARATOR = ",";
+    private static final String RANGE_IDENTIFIER = "..";
+    private static final String AGGREGATION_IDENTIFIER = "(";
+    // aggregation to add: unique, percentile, sumsq
+    private static final String[] AGGREGATION_FUNCTIONS = {"sum", "avg", "max", "min", "unique", "variance", "stddev"};
+    public static final Pattern CATEGORICAL_PATTERN = Pattern.compile("^([a-zA-Z][a-zA-Z0-9_.]+)(\\[[a-zA-Z0-9_.\\-,*]+])?(:\\*|:\\d+)?$");
+
+    private Param<String> facetParam;
+
+    private Map<String, String> validCategoricalFields;
+    private Map<String, String> validRangeFields;
+    private Set<String> isExplode;
+
+    public FacetTransformer() {
+        this(null);
+    }
+
+    public FacetTransformer(String uid) {
+        super(uid);
+        facetParam = new Param<>(this, "facet", "");
+
+        init();
+    }
+
+    // Study ID parameter
+    public Param<String> facetParam() {
+        return facetParam;
+    }
+
+    public FacetTransformer setFacet(String facetParam) {
+        set(facetParam(), facetParam);
+        return this;
+    }
+
+    @Override
+    public Dataset<Row> transform(Dataset<?> df) {
+        Dataset<Row> res = (Dataset<Row>) df;
+
+
+        // Sanity check
+        if (facetParam() == null) {
+            return df.sparkSession().emptyDataFrame();
+        }
+
+        String facet = get(facetParam()).get();
+        if (facet.contains(NESTED_FACET_SEPARATOR)) {
+            // Nested facet
+            String[] facets = facet.split(NESTED_FACET_SEPARATOR);
+
+            // Sanity check
+            for (int i = 0; i < facets.length - 1; i++) {
+                if (getFacetType(facets[i]) == FacetType.AGGREGATION) {
+                    throw new InvalidParameterException("In nested facets, aggregations must be in last place: " + facet);
+                }
+            }
+            List<String> fieldNames = new LinkedList<>();
+            List<String> facetNames = new LinkedList<>();
+            for (String simpleFacet: facets) {
+                String fieldName = getFieldName(simpleFacet);
+                if (fieldNames.contains(fieldName)) {
+                    throw new InvalidParameterException("In nested facets, repeating facets are not allowed: " + facet);
+                }
+                fieldNames.add(fieldName);
+            }
+
+            // Process facet
+            boolean aggregation = false;
+            res = (Dataset<Row>) df;
+            for (int i = 0; i < fieldNames.size(); i++) {
+                switch (getFacetType(facets[i])) {
+                    case CATEGORICAL: {
+                        // Categorical facet
+                        String facetName = fieldNames.get(i);
+                        res = processCategoricalFacet(facets[i], fieldNames.get(i), facetName, res);
+                        facetNames.add(facetName);
+                        break;
+                    }
+                    case RANGE: {
+                        // Range facet
+                        String facetName = fieldNames.get(i) + "Range";
+                        res = processRangeFacet(facets[i], fieldNames.get(i), facetName, res);
+                        facetNames.add(facetName);
+                        break;
+                    }
+                    case AGGREGATION: {
+                        // Aggregation must be the last one!!!
+                        res = processAggregationFacet(facets[i], fieldNames.get(i), res);
+                        aggregation = true;
+                        break;
+                    }
+                    default: {
+                        throw new InvalidParameterException("In nested facets, unknown facet in middle position: " + facet);
+                    }
+                }
+            }
+
+            Column[] cols = new Column[facetNames.size()];
+            for (int i = 0; i < facetNames.size(); i++) {
+                cols[i] = new Column(facetNames.get(i));
+            }
+            if (aggregation) {
+                // Special case, we have aggregations
+                res = res.groupBy(cols).agg(expr(facets[facets.length - 1]), count(lit(1)).as("count")).orderBy(cols);
+            } else {
+                res = res.groupBy(cols).count().orderBy(cols);
+            }
+        } else {
+            // Simple facet: categorical, range or aggregation
+            String fieldName = getFieldName(facet);
+            switch (getFacetType(facet)) {
+                case CATEGORICAL: {
+                    // Categorical facet
+                    String facetName = fieldName;
+                    Dataset<Row> categoricalDf = processCategoricalFacet(facet, fieldName, facetName, (Dataset<Row>) df);
+                    res = categoricalDf.groupBy(facetName).count().orderBy(facetName);
+                    break;
+                }
+                case RANGE: {
+                    // Range facet
+                    String facetName = fieldName + "Range";
+                    Dataset<Row> rangeDf = processRangeFacet(facet, fieldName, facetName, (Dataset<Row>) df);
+                    res = rangeDf.groupBy(facetName).count().orderBy(facetName);
+                    break;
+                }
+                case AGGREGATION: {
+                    // Aggregation facet
+                    Dataset<Row> cached = processAggregationFacet(facet, fieldName, (Dataset<Row>) df);
+                    long count = cached.count();
+                    res = cached.agg(expr(facet)).withColumn("count", lit(count));
+                    break;
+                }
+                default:
+                    throw new InvalidParameterException("Unknown facet in middle position: " + facet);
+            }
+        }
+
+        // Save facet in metadata
+        Metadata facetMetadata = new MetadataBuilder().putString("facet", facet).build();
+        return res.withColumn("count", col("count").as("count", facetMetadata));
+    }
+
+    private Dataset<Row> processCategoricalFacet(String facet, String fieldName, String facetName, Dataset<Row> df) {
+        Dataset<Row> res = df;
+        if (validCategoricalFields.containsKey(fieldName)) {
+            if (isExplode.contains(fieldName)) {
+                res = res.withColumn(facetName, getColumn(fieldName));
+            }
+            List<String> values = getIncludeValues(facet);
+            if (ListUtils.isNotEmpty(values)) {
+                StringBuilder include = new StringBuilder(facetName).append("='").append(values.get(0)).append("'");
+                for (int i = 1; i < values.size(); i++) {
+                    include.append(" OR ").append(facetName).append("='").append(values.get(i)).append("'");
+                }
+                res = res.filter(include.toString());
+            }
+        }
+        return res;
+    }
+
+    private Dataset<Row> processRangeFacet(String facet, String fieldName, String facetName, Dataset<Row> df) {
+        // Parse range
+        String[] split = facet.replace("[", ":").replace("..", ":").replace("]", "").split(":");
+        double start = Double.parseDouble(split[1]);
+        double end = Double.parseDouble(split[2]);
+        double step = Double.parseDouble(split[3]);
+
+        UserDefinedFunction scoreFunction = udf(new ScoreFunction(fieldName), DataTypes.DoubleType);
+        ListBuffer<Column> functScoreSeq = createFunctScoreSeq(fieldName);
+
+        return df.withColumn(facetName, scoreFunction.apply(functScoreSeq).divide(step).cast(DataTypes.IntegerType)
+                .multiply(step)).filter(facetName + ">= " + start + " AND " + facetName + " <= " + end);
+    }
+
+    private Dataset<Row> processAggregationFacet(String facet, String fieldName, Dataset<Row> df) {
+        // Validate aggregation function
+        String aggFunction = facet.substring(0, facet.indexOf("("));
+        boolean found = false;
+        for (String agg: AGGREGATION_FUNCTIONS) {
+            if (agg.equals(aggFunction)) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            throw new InvalidParameterException("Aggregation function unknown: " + aggFunction);
+        }
+
+        UserDefinedFunction scoreFunction = udf(new ScoreFunction(fieldName), DataTypes.DoubleType);
+        ListBuffer<Column> functScoreSeq = createFunctScoreSeq(fieldName);
+        return df.withColumn(fieldName, scoreFunction.apply(functScoreSeq));
+    }
+
+    private ListBuffer<Column> createFunctScoreSeq(String fieldName) {
+        if (fieldName.equals("cadd_scaled") || fieldName.equals("cadd_raw")) {
+            return new ListBuffer<Column>().$plus$eq(col("annotation.functionalScore"));
+        } else {
+            return new ListBuffer<Column>().$plus$eq(col("annotation.conservation"));
+        }
+    }
+
+    @Override
+    public StructType transformSchema(StructType schema) {
+        return schema;
+//        List<StructField> fields = new ArrayList<>();
+//        fields.add(createStructField("id", DoubleType, false));
+//        fields.add(createStructField("type", DoubleType, false));
+//        fields.add(createStructField("phastCons", DoubleType, false));
+//        fields.add(createStructField("phylop", DoubleType, false));
+//        fields.add(createStructField("gerp", DoubleType, false));
+//        fields.add(createStructField("caddRaw", DoubleType, false));
+//        fields.add(createStructField("caddScaled", DoubleType, false));
+//        return createStructType(fields);
+    }
+
+    public static class ScoreFunction extends AbstractFunction1<WrappedArray<GenericRowWithSchema>,
+            Double> implements Serializable {
+        private String source;
+
+        public ScoreFunction(String source) {
+            this.source = source;
+        }
+
+        @Override
+        public Double apply(WrappedArray<GenericRowWithSchema> functionalScores) {
+            for (int i = 0; i < functionalScores.length(); i++) {
+                Row functScore = functionalScores.apply(i);
+                if (functScore.apply(1).equals(source)) {
+                    return Double.parseDouble(functScore.apply(0).toString());
+                }
+            }
+
+            return Double.NEGATIVE_INFINITY;
+        }
+    }
+
+    private void init() {
+        // Categorical fields
+        validCategoricalFields = new HashMap<>();
+        validCategoricalFields.put("chromosome", "chromosome");
+        validCategoricalFields.put("names", "names");
+        validCategoricalFields.put("reference", "reference");
+        validCategoricalFields.put("alternate", "alternate");
+        validCategoricalFields.put("strand", "strand");
+        validCategoricalFields.put("type", "type");
+        validCategoricalFields.put("biotype", "annotation.consequenceTypes.biotype");
+        validCategoricalFields.put("gene", "annotation.consequenceTypes.geneName");
+        validCategoricalFields.put("ensemblGeneId", "annotation.consequenceTypes.ensemblGeneId");
+        validCategoricalFields.put("ensemblTranscriptId", "annotation.consequenceTypes.ensemblTranscriptId");
+
+        // Is explode?
+        isExplode = new HashSet<>();
+        isExplode.add("names");
+        isExplode.add("biotype");
+        isExplode.add("gene");
+        isExplode.add("ensemblGeneId");
+        isExplode.add("ensemblTranscriptId");
+
+        // Range fields
+        validRangeFields = new HashMap<>();
+        validRangeFields.put("gerp", "annotation.conservation");
+        validRangeFields.put("phylop", "annotation.conservation");
+        validRangeFields.put("phastCons", "annotation.conservation");
+        validRangeFields.put("cadd_scaled", "annotation.functionalScore");
+        validRangeFields.put("cadd_raw", "annotation.functionalScore");
+    }
+
+    private Column getColumn(String facetName) {
+        if (isExplode.contains(facetName)) {
+            switch(facetName) {
+                case "gene":
+                    return explode(genes("annotation"));
+                case "biotype":
+                    return explode(biotypes("annotation"));
+                default:
+                    return explode(col(validCategoricalFields.get(facetName)));
+            }
+
+        } else {
+            return col(validCategoricalFields.get(facetName));
+        }
+    }
+
+    private List<String> getIncludeValues(String facet) {
+        // Categorical...
+        if (facet.contains("[")) {
+            Matcher matcher = CATEGORICAL_PATTERN.matcher(facet);
+            if (matcher.find()) {
+                String include = matcher.group(2).replace("[", "").replace("]", "");
+
+                if (StringUtils.isNotEmpty(include)) {
+                    if (!include.contains("*")) {
+                        return Arrays.asList(include.split(INCLUDE_SEPARATOR));
+                    }
+                }
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    private String getFieldName(String facet) {
+        if (facet.contains(AGGREGATION_IDENTIFIER)) {
+            return facet.substring(facet.indexOf('(') + 1, facet.indexOf(')'));
+        } else {
+            if (facet.contains("[")) {
+                return facet.substring(0, facet.indexOf('['));
+            } else {
+                return facet;
+            }
+        }
+    }
+
+    private FacetType getFacetType(String facet) {
+        if (facet.contains(RANGE_IDENTIFIER)) {
+            return FacetType.RANGE;
+        } else if (facet.contains(AGGREGATION_IDENTIFIER)) {
+            return FacetType.AGGREGATION;
+        } else {
+            return FacetType.CATEGORICAL;
+        }
+    }
+}
---
+++ oskar-spark/src/test/java/org/opencb/oskar/spark/variant/analysis/FacetTransformerTest.java
@@ -0,0 +1,165 @@
+package org.opencb.oskar.spark.variant.analysis;
+
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
+import org.apache.spark.sql.expressions.UserDefinedFunction;
+import org.apache.spark.sql.types.*;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.opencb.biodata.models.feature.Genotype;
+import org.opencb.oskar.analysis.variant.FisherExactTest;
+import org.opencb.oskar.analysis.variant.MendelianError;
+import org.opencb.oskar.spark.OskarSparkTestUtils;
+import org.opencb.oskar.spark.commons.OskarException;
+import org.opencb.oskar.spark.variant.udf.StudyFunction;
+import scala.collection.mutable.ListBuffer;
+import scala.collection.mutable.WrappedArray;
+import scala.runtime.AbstractFunction1;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import static org.apache.spark.sql.functions.*;
+import static org.opencb.oskar.spark.variant.converters.VariantToRowConverter.STUDY_ID_IDX;
+import static org.opencb.oskar.spark.variant.udf.VariantUdfManager.biotypes;
+import static org.opencb.oskar.spark.variant.udf.VariantUdfManager.consequenceTypes;
+import static org.opencb.oskar.spark.variant.udf.VariantUdfManager.genes;
+
+public class FacetTransformerTest {
+
+    @ClassRule
+    public static OskarSparkTestUtils sparkTest = new OskarSparkTestUtils();
+
+    @Test
+    public void emptyFacet() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        FacetTransformer facetTransformer = new FacetTransformer();
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show();
+    }
+
+    @Test
+    public void simpleFacet() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        String facet = "type";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show();
+    }
+
+    @Test
+    public void simpleAndIncludeFacet() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        String facet = "biotype[protein_coding,miRNA,retained_intron]";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show();
+    }
+
+    @Test
+    public void rangeFacet() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        String facet = "gerp[-3..3]:1";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show();
+    }
+
+    @Test
+    public void aggregationFacet() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        String facet = "avg(gerp)";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show();
+    }
+
+    @Test
+    public void nestedFacetCatAndCat() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        String facet = "biotype>>type";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show();
+    }
+
+    @Test
+    public void nestedFacetCatAndCatAndRange() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+
+        String facet = "biotype>>type>>gerp[-10..10]:0.5";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show(100);
+    }
+
+    @Test
+    public void nestedFacetCatAndCatAndRangeAndRange() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+        String facet = "cadd_raw[-100..100]:10>>biotype>>type>>gerp[-10..10]:0.5>>gene[CNN2P1,EIF4ENIF1,IGLV3-12,CTA-85E5.10]";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show(100);
+    }
+
+    @Test
+    public void nestedFacetCatAndAggAndCat() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+        String facet = "biotype>>avg(gerp)>>type";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show(100);
+    }
+
+    @Test
+    public void nestedFacetCatAndCatAndAgg() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+        String facet = "biotype>>type>>avg(gerp)";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show(100);
+    }
+
+    @Test
+    public void nestedFacetCatAndRangeAndAgg() throws IOException, OskarException {
+        Dataset<Row> df = sparkTest.getVariantsDataset();
+        String facet = "gene>>biotype>>cadd_raw[-100..100]:20>>cadd_scaled[-100..100]:20>>avg(gerp)";
+        FacetTransformer facetTransformer = new FacetTransformer();
+        facetTransformer.setFacet(facet);
+
+        Dataset<Row> res = facetTransformer.transform(df);
+        res.show(100);
+
+        System.out.println(res.schema().apply("count").metadata().getString("facet"));
+    }
+}
